8df556a6f9aba2960c6acf869822bed52f07ec10
jasonmilli <jasonmllngtn@gmail.com>
2015-07-02 21:09:35 +0000

    lindbg

--- linear.c
+++ linear.c
@@ -17,9 +17,9 @@ void creatematrix() {
             k++;
             if (t > (1 << n)) {
                 printf("All rows in the generator matrix (g) must by shorter than the length (n) argument");
+                free(a);
                 exit(0);
             }
-            //int *a = binary(t);
             int b[n];
             for (l = 0; l < n; l++) {
                 if (t >= (1 << (n - l - 1))) {
@@ -27,22 +27,38 @@ void creatematrix() {
                     t -= (1 << (n - l - 1));
                 } else b[l] = 0;
             }
+            /*int mmm;
+            for (mmm = 0; mmm < sizeof(n); mmm++) printf("b%d: %d", ii, b[mmm]);
+            printf("\n");*/
             a = realloc(a, arrsize * (ii + 1));
             a[ii] = b;
             ii++;
-            printf("%d\n", t);
+            //printf("%d\n", t);
             t = j = 0;
         } else {
             t += (g[i] - '0') * pow(10,j);
             j++;
         }
     }
+    int m,mm;
+    for (m = 0; m < sizeof(a) / sizeof(a[0]); m++) {
+        for (mm = 0; mm < sizeof(a[m]); mm++) printf("%d ", a[m][mm]);
+        printf("\n");
+    }
 }
 void decode() {
 }
 void encode() {
+    int iii = 0, tt, jjj;
     while ((c = getc(stdin)) != EOF) {
-        
+        if (iii % k == 0) {
+            tt = 0;
+            if (iii != 0) printf("%d", (tt & 1));
+        }
+        //for (jjj = 0; jjj < n; jjj++) tt += a[iii % k][jjj];
+        iii++;
+    }
+    printf("%d", (tt & 1));
 }
 void main(int argc, char **argv) {
     while ((c = getopt(argc, argv, "n:g:d")) != -1) {
